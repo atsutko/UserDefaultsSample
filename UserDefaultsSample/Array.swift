c3158efa16b77a25dcda673e31bf3ddae02dd26d
atsutko <atsushi.takao.66@gmail.com>
2018-07-07 11:16:27 +0900

    first comit

---
+++ UserDefaultsSample/Array.swift
@@ -0,0 +1,20 @@
+//
+//  Array.swift
+//  UserDefaultsSample
+//
+//  Created by TakaoAtsushi on 2018/07/07.
+//  Copyright © 2018年 TakaoAtsushi. All rights reserved.
+//
+
+import UIKit
+
+class Array: NSObject {
+
+    var title: String
+    var image: UIImage
+    
+    init(title: String, image: UIImage) {
+        self.title = title
+        self.image = image
+    }
+}
